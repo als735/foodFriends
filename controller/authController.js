43d4e0fc0b26462a335af85a3e4ea99a5e67484e
Abby Smith <smith@Abbys-MacBook-Pro.local>
2019-06-15 17:41:08 -0600

    Saturday mid-evening commit

--- controller/authController.js
+++ controller/authController.js
@@ -1,10 +1,12 @@
 const bcrypt = require('bcrypt');
 const saltRounds = 10
+// const session = require("express-session");
+
 
 module.exports = {
     login: (req, res, next) => {
         const {session} = req; 
-        let {email, password} = req.body; 
+        let {email, user_password} = req.body; 
         let db = req.app.get('db')
         let currentUser;
         // check for user by email
@@ -14,29 +16,53 @@ module.exports = {
                 if(user){
                     currentUser = user; 
                 //checks for correct password 
-                return bcrypt.compare(password, user.user_password)
+                return bcrypt.compare(user_password, user.user_password)
             } else {
                 throw("User does not exist!")
             }
         }) //chain a promise 
-        .then((user)=>{
-            if(user){
+        .then((isMatch)=>{
+            if(isMatch){
                 delete currentUser.user_password
+
                 req.session.user = currentUser
-                //send back response 
+                // send back response
                 res.send({success: true, user:currentUser})
-            } else {
-                throw ('Incorrect email or password')
+            }else{
+                throw('Incorrect credentials')
             }
         })
         .catch((err)=>{
-            res.send({success: false, err})
+            //If incorrect send back failed loging
+            res.send({success:false, err})
         })
 
     }, 
 
     register: (req, res, next) => {
+        const db = req.app.get('db');
 
+        const {email, user_password, first_name, last_name, calories, net_carbs, fat, protein} = req.body; 
+
+        db.users.findOne({email})
+            .then((user)=>{
+                if(user){
+                    throw("This user already exists. Please login.")
+                }else {
+                    return bcrypt.hash(user_password, saltRounds);
+                }
+            })
+            .then((hash)=> {
+                return db.users.insert({email, user_password:hash,first_name,last_name,calories,net_carbs,fat,protein})
+            })
+            .then((user)=>{
+                delete user.user_password; 
+                req.session.user = user; 
+                res.send({success: true, user:user})
+            })
+            .catch((err) => {
+                res.send({success:false, err})
+            })
 
     },
     authMe: (req, res, next) => {
